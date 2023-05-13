f32b67cb820f26ca7b4d2e6fb0452fdc857199e7
nawodyaishan <nawodyain@gmail.com>
2023-05-14 03:17:26 +0530

    feat: add angular user component

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,10 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+export interface User {
+  name: string;
+  age: number;
+  address: string;
+}
+
+export interface QueryResponse {
+  data: { users: User[] };
+}
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get<QueryResponse>('http://localhost:4000/graphql?query={ users { name age address } }').subscribe(data => {
+      this.users = data.data.users;
+    }, error => {
+      console.log(error);
+    });
+  }
 
 }
